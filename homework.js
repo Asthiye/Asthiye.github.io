c0403bd93ae1eb92a45677310130e7297d77a6c9
Asthiye <126553748+Asthiye@users.noreply.github.com>
2023-05-07 00:14:54 +0800

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,70 @@
+document.write("Hello,world!");
+function tips(){
+  alert("Write down Chinese singer's name below every album cover in Chinese");
+}
+
+function answer1(){
+  var response = prompt("What's the singer's name?Please weite down it in Chinese.");
+  var x = response
+  if(x === "周杰伦"){
+    alert("Your answer is right!")
+  }
+  else{
+    alert("Sorry,the singer's name is “周杰伦”")
+  }
+}
+
+function answer2(){
+  var response = prompt("What's the singer's name?Please weite down it in Chinese.");
+  var x = response
+  if(x === "林俊杰"){
+    alert("Your answer is right!")
+  }
+  else{
+    alert("Sorry,the singer's name is “林俊杰”")
+  }
+}
+
+function answer3(){
+  var response = prompt("What's the singer's name?Please weite down it in Chinese.");
+  var x = response
+  if(x === "艾怡良"){
+    alert("Your answer is right!")
+  }
+  else{
+    alert("Sorry,the singer's name is “艾怡良”")
+  }
+}
+
+function answer4(){
+  var response = prompt("What's the singer's name?Please weite down it in Chinese.");
+  var x = response
+  if(x === "杨和苏"){
+    alert("Your answer is right!")
+  }
+  else{
+    alert("Sorry,the singer's name is “杨和苏”")
+  }
+}
+
+function answer5(){
+  var response = prompt("What's the singer's name?Please weite down it in Chinese.");
+  var x = response
+  if(x === "王心凌"){
+    alert("Your answer is right!")
+  }
+  else{
+    alert("Sorry,the singer's name is “王心凌”")
+  }
+}
+
+function answer6(){
+  var response = prompt("What's the singer's name?Please weite down it in Chinese.");
+  var x = response
+  if(x === "林忆莲"){
+    alert("Your answer is right!")
+  }
+  else{
+    alert("Sorry,the singer's name is “林忆莲”")
+  }
+}
